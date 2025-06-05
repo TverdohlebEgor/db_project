b8d2e1df2beb98415e794dfd2b8bd0fb7d0ec1fd
PietroMaro <pietro.maretti@icloud.com>
2025-06-05 12:34:37 +0200

    Minor fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,16 +8,6 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div>
-      <h2>Login</h2>
-      <form action="http://localhost:8080/api/login" method="POST">
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required /><br /><br />
-
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required /><br /><br />
-
-        <input type="submit" value="Accedi" />
-      </form>
     </div>
   );
 }
